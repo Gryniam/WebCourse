e3f3b9c2f9928cba00c931a4187aeff1c056aace
Gryniam <muzikaeng@gmail.com>
2022-06-01 10:53:44 +0300

    Fix

--- Lab9/js/Hide__Menu.js
+++ Lab9/js/Hide__Menu.js
@@ -2,11 +2,10 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    document.querySelector(".navbar-toggler").addEventListener("blur", //Втрата фокуса на об'єкті
+    document.querySelector(".navbar-toggler").addEventListener("blur",
         function (event) {
             var screenWidth = window.innerWidth;
             if (screenWidth < 768) {
-                // document.getElementById("navbarToggler").classList.remove("show"); // Грубий варіант згортання меню
                 let tempCollapse = new bootstrap.Collapse(document.getElementById("navbarToggler"), {toggle: true});
             }
         });
