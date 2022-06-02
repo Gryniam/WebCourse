2bba3f039104571457733f10f6576a332c436cc2
Gryniam <muzikaeng@gmail.com>
2022-06-02 10:20:25 +0300

    Changed Lab7

--- Lab7/js/script.js
+++ Lab7/js/script.js
@@ -6,272 +6,191 @@ console.log('adjacent angle        прилеглий до катета кут')
 console.log('opposite angle        протилежний до катета кут');
 console.log('angle                 один з двох гострих кутів(коли задана гіпотенуза)');
 
-var myTriangle = {
-    firstleg: 0,
-    secondleg: 0,
-    hypotenuse: 0,
-    adjacent_angle: 0,
-    opposite_angle: 0
+var type1=true, type2=true;
 
-};
 
-function  triangle(first, first1, second, second1){
-    let haveHipotenuse = false;
-    if(validationByParams(first, first1, second, second1)){
-        haveHipotenuse = getHipotenuseStatus(first, first1, second, second1);
-        fillExistParams(first, first1, second, second1);
-        console.log("ВАЛІДАЦІЯ ПО ВВОДУ ПРОЙШЛА УСПІШНО");
-        if(validationByMath(myTriangle)){
-            if(haveHipotenuse){
-                getDataWithHypotenuse(myTriangle);
-            }
-            if(!haveHipotenuse){
-                getDataWithoutHipotenuse(myTriangle);
+function check_negative(argument_1, type_1, argument_2, type_2){
+    //від'ємні аргументи
+    if(argument_1 <= 0){
+        if(argument_1 == 0){ console.log(type_1+" не може бути рівним 0.");}
+        else if (argument_1 < 0){ console.log(type_1+" не може бути меншим 0.");}
+        type1=false;}
 
-            }
+    if(argument_2 <= 0){
+        if(argument_2 == 0){console.log(type_2+" не може бути рівним 0.");}
+        else if (argument_2 < 0){console.log(type_2+" не може бути меншим 0.");}
+        type2=false;}
+}
 
+function check_hypo_leg(argument_1, type_1, argument_2, type_2){
+    //гіпотенуза менша або рівна катету
+    if(type_1=="leg"&&type_2=="hypotenuse"){
+        if(argument_1>argument_2){console.log(type_1+" більший за "+ type_2); type1=false;}
+        else if(argument_1==argument_2){ console.log(type_1+" рівний "+ type_2); type1=false;}
+    }
+
+    if(type_1=="hypotenuse"&&type_2=="leg"){
+        if(argument_2>argument_1){console.log(type_2+" більший за "+ type_1);type2=false;}
+        else if(argument_1==argument_2){console.log(type_1+" рівний "+ type_2);type2=false;}
+    }
+}
+
+function check_blunt_angle(argument_1, type_1, argument_2, type_2){
+    //кути є негострими
+    if(type_1=="adjacent angle"){
+        if(argument_1>89){ console.log(type_1+" є негострим кутом");
+            type1=false;}
+    }
+    if(type_2=="adjacent angle"){
+        if(argument_2>89){ console.log(type_2+" є негострим кутом");
+            type2=false;}
+    }
+    if(type_1=="opposite angle"){
+        if(argument_1>89){console.log(type_1+" є негострим кутом");
+            type1=false; }
+    }
+    if(type_2=="opposite angle"){
+        if(argument_2>89){console.log(type_2+" є негострим кутом");
+            type2=false;}
+    }
+    if(type_1=="angle"){
+        if(argument_1>89){console.log(type_1+" є негострим кутом");
+            type1=false;}
+    }
+    if(type_2=="angle"){
+        if(argument_2>89){console.log(type_2+" є негострим кутом");
+            type2=false;}
+    }
+}
+
+
+function triangle(argument_1, type_1, argument_2, type_2){
+    type1=true
+    type2=true;
+    var a=5, b=9, c=10.3, alpha=29.0546, beta=60.9454;
+    //неправилдльна к-сть аргументів введено
+    if (argument_1 == undefined || type_1 == undefined || argument_2== undefined || type_2 == undefined ) {
+        console.log("failed!");
+        return;
+    }
+
+// помилка в парі параметрів або назві параметра
+    if(type_1=="leg"&&type_2=="leg"||type_1=="leg"&&type_2=="hypotenuse"||type_2=="leg"&&type_1=="hypotenuse" ||type_1=="leg"&&type_2=="opposite angle"||type_1=="opposite angle"&&type_2=="leg" ||type_1=="leg"&&type_2=="adjacent angle"||type_1=="adjacent angle"&&type_2=="leg" ||type_1=="hypotenuse"&&type_2=="angle"||type_1=="angle"&&type_2=="hypotenuse")
+    {
+        //з прямим порядком аргументів
+        if(type_1 == "leg" && type_2 == "leg") {
+            check_negative(argument_1, type_1, argument_2, type_2);
+            if(type1){a = argument_1;}
+            if(type2){b = argument_2;}
+            if(!type1){console.log("Використовується значення за замовчуванням a = "+a);}
+            if(!type2){console.log("Використовується значення за замовчуванням b = "+b);}
+            c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+            alpha = Math.asin(a/c)*180/Math.PI;
+            beta = Math.acos(a/c)*180/Math.PI;
         }
+        else if(type_1 == "leg" && type_2 == "hypotenuse") {
+            check_negative(argument_1, type_1, argument_2, type_2);
+            if(type1){a = argument_1;}
+            if(type2){c = argument_2;}
+
+            check_hypo_leg(a, type_1, c, type_2);
+            if(a>c){c = 10.3;}
+            if(type1&&type2){a=argument_1;c=argument_2;}
+            else{console.log("Використовується значення за замовчуванням a = "+a+" c = "+c);}
+            b=Math.sqrt(Math.pow(c,2)-Math.pow(a,2));
+            alpha = Math.asin(a/c)*180/Math.PI;
+            beta = Math.acos(a/c)*180/Math.PI;
+        }
+        else if(type_1 == "leg" && type_2 == "opposite angle"){
+            check_negative(argument_1, type_1, argument_2, type_2);
+            if(type1){a = argument_1;}
+            if(!type1){console.log("Використовується значення за замовчуванням a = "+a);}
+            check_blunt_angle(argument_1, type_1, argument_2, type_2);
+            if(type2){ alpha=argument_2;}
+            else{console.log("Використовується значення за замовчуванням alpha = "+alpha);}
+            c=a / (Math.sin(alpha * Math.PI/180));
+            b=Math.sqrt(Math.pow(c,2)-Math.pow(a,2));
+            beta = 90 - alpha;
+        }
+        else if(type_1 == "leg" && type_2 == "adjacent angle"){
+            check_negative(argument_1, type_1, argument_2, type_2);
+            if(type1){a = argument_1;}
+            if(!type1){console.log("Використовується значення за замовчуванням a = "+a);}
+            check_blunt_angle(argument_1, type_1, argument_2, type_2);
+            if(type2){ beta=argument_2;}
+            else{console.log("Використовується значення за замовчуванням beta = "+beta);}
+            c = a / (Math.cos(beta * Math.PI/180));
+            b = Math.sqrt(Math.pow(c, 2) - Math.pow(a, 2));
+            alpha = 90 - beta;
+        }
+
+        else if(type_1 == "hypotenuse" && type_2 == "angle"){
+            check_negative(argument_1, type_1, argument_2, type_2);
+            if(type1){c = argument_1;}
+            if(!type1){console.log("c<0 використовується значення за замовчуванням c = "+c);}
+            check_blunt_angle(argument_1, type_1, argument_2, type_2);
+            if(type2){ alpha=argument_2;}
+            else{console.log("Використовується значення за замовчуванням alpha = "+alpha);}
+
+            beta = 90  - alpha;
+            a=  c * Math.sin(alpha * Math.PI/180);
+            b= c * Math.sin(beta * Math.PI/180);
+        }
+////////// з оберненим порядком аргументів
+
+        else if(type_1 == "hypotenuse" && type_2 == "leg") {
+            check_negative(argument_1, type_1, argument_2, type_2);
+            if(type1){c = argument_1;}
+            if(type2){a = argument_2;}
+
+            check_hypo_leg(c, type_1, a, type_2);
+            if(a>c){c = 10.3;}
+            if(type1&&type2){c=argument_1;a=argument_2;}
+            else{console.log("Використовується значення за замовчуванням a = "+a+" c = "+c);}
+            b=Math.sqrt(Math.pow(c,2)-Math.pow(a,2));
+            alpha = Math.asin(a/c)*180/Math.PI;
+            beta = Math.acos(a/c)*180/Math.PI;
+        }
+        else if(type_1 == "opposite angle" && type_2 == "leg"){
+            check_negative(argument_1, type_1, argument_2, type_2);
+            if(type2){a = argument_2;}
+            if(!type2){console.log("Використовується значення за замовчуванням a = "+a);}
+            check_blunt_angle(argument_1, type_1, argument_2, type_2);
+            if(type1){alpha=argument_1;}
+            else{console.log("Використовується значення за замовчуванням alpha = "+alpha);}
+            c=a / (Math.sin(alpha * Math.PI/180));
+            b=Math.sqrt(Math.pow(c,2)-Math.pow(a,2));
+            beta = 90 - alpha;
+        }
+        else if(type_1 == "adjacent angle" && type_2 == "leg"){
+            check_negative(argument_1, type_1, argument_2, type_2);
+            if(type2){a = argument_1;}
+            if(!type2){console.log("Використовується значення за замовчуванням a = "+a);}
+            check_blunt_angle(argument_1, type_1, argument_2, type_2);
+            if(type1){ beta=argument_2;}
+            else{console.log("Використовується значення за замовчуванням beta = "+beta);}
+            c = a / (Math.cos(beta * Math.PI/180));
+            b = Math.sqrt(Math.pow(c, 2) - Math.pow(a, 2));
+            alpha = 90 - beta;
+        }
+
+        else if(type_1 == "angle" && type_2 == "hypotenuse"){
+            check_negative(argument_1, type_1, argument_2, type_2);
+            if(type2){c = argument_2;}
+            if(!type2){console.log("c<0 використовується значення за замовчуванням c = "+c);}
+            check_blunt_angle(argument_1, type_1, argument_2, type_2);
+            if(type1){ alpha=argument_1;}
+            else{console.log("Використовується значення за замовчуванням alpha = "+alpha);}
+            beta = 90  - alpha;
+            a=  c * Math.sin(alpha * Math.PI/180);
+            b= c * Math.sin(beta * Math.PI/180);
+        }
+
+
         else{
-            console.log("Некорректні дані трикутника, він не є прямокутним")
-        }
-
-    }
-    myTriangle.firstleg = 0;
-    myTriangle.opposite_angle = 0;
-    myTriangle.adjacent_angle = 0;
-    myTriangle.hypotenuse = 0;
-    myTriangle.secondleg = 0;
-}
-
-
-//Валідація по вводу
-function validationByParams(first, first1, second, second1){
-    if(isParametersCorrect(first, first1, second, second1)){
-
-    }
-    else {
-        console.log("Параметри введені не в правильному порядку")
-        console.log("failed")
-        return false;
-    }
-
-    if(isSrtingExists(first, first1, second, second1)){
-
-    }
-    else{
-        console.log("Перевірте чи введені параметри корректні")
-        return  false;
-    }
-    return true;
-}
-function isParametersCorrect(first, first1, second, second1){
-    if(typeof(first) == "number" && typeof (first1) == "string"&& typeof (second) == "number" && typeof(second1) == "string"){
-        return true;
-    }
-    if(typeof(first) == "string" && typeof (first1) == "number"&& typeof (second) == "string" && typeof(second1) == "number"){
-        return true;
-    }
-    return false;
-}
-function isSrtingExists(first, first1, second, second1){
-    let existParams = ["leg","hypotenuse","adjacent angle","opposite angle","angle"];
-
-    let firstParamCorrect = false;
-    let secondParamCorrect = false;
-
-    if(typeof(first) == "string"){
-        for(let i =0; i<5; i++){
-            if(first === existParams[i]){
-                firstParamCorrect = true;
-            }
-            if(second == existParams[i]){
-                secondParamCorrect = true;
-            }
+            console.log("failed!");
+            return;
         }
     }
-    else{
-        for(let i =0; i<5; i++){
-            if(first1 === existParams[i]){
-                firstParamCorrect = true;
-            }
-            if(second1 == existParams[i]){
-                secondParamCorrect = true;
-            }
-        }
-    }
-
-    if(firstParamCorrect && secondParamCorrect){
-        return true;
-    }
-    else{
-        return  false;
-    }
-
-}
-
-function fillExistParams(first, first1, second, second1){
-    if(typeof(first) == "string"){
-        if(first === "leg"){
-            myTriangle.firstleg = first1;
-        }
-        if(first === "hypotenuse"){
-            myTriangle.hypotenuse = first1;
-        }
-        if(first === "adjacent angle"){
-            myTriangle.adjacent_angle =getRadian(first1);
-        }
-        if(first === "opposite angle" || first1 === "angle"){
-            myTriangle.opposite_angle = getRadian(first1);
-        }
-        if(second === "leg"){
-            myTriangle.firstleg = second1;
-        }
-        if(second === "hypotenuse"){
-            myTriangle.hypotenuse = second1;
-        }
-        if(second === "adjacent_angle"){
-            myTriangle.adjacent_angle = getRadian(second1);
-        }
-        if(second === "opposite angle"|| first1 === "angle"){
-            myTriangle.opposite_angle = getRadian(second1);
-        }
-    }
-    else{
-        if(first1 === "leg"){
-            myTriangle.firstleg = first;
-        }
-        if(first1 === "hypotenuse"){
-            myTriangle.hypotenuse = first;
-        }
-        if(first1 === "adjacent angle"){
-            myTriangle.adjacent_angle = getRadian(first);
-        }
-        if(first1 === "opposite angle"|| first1 === "angle"){
-            myTriangle.opposite_angle = getRadian(first);
-        }
-        if(second1 === "leg"){
-            myTriangle.firstleg = second;
-        }
-        if(second1 === "hypotenuse"){
-            myTriangle.hypotenuse = second;
-        }
-        if(second1 === "adjacent angle"){
-            myTriangle.adjacent_angle = getRadian(second);
-        }
-        if(second1 === "opposite angle"|| first1 === "angle"){
-            myTriangle.opposite_angle = getRadian(second);
-        }
-    }
-
-}
-////////
-
-
-//Перевірка на гіпотенузу
-function getHipotenuseStatus(first, first1, second, second1){
-    let params = [first, first1, second,second1];
-    for(var i = 0; i <4; i++){
-        if(params[i] === 'hypotenuse') {
-            return true;
-        }
-    }
-    return  false;
-}
-
-//Конвертить радіани в градуси
-function getGradus(radian){
-    return radian * 180/Math.PI;
-}
-function getRadian(gradus){
-    return (gradus * Math.PI)/180;
-}
-
-
-
-function validationByMath(myTriangle){
-    let hipotenuse = myTriangle.hypotenuse;
-
-    if(myTriangle.firstleg > hipotenuse && myTriangle.hypotenuse != 0){
-        return false;
-    }
-    if(getGradus(myTriangle.adjacent_angle) > 90 || getGradus(myTriangle.opposite_angle) > 90){
-        return  false;
-    }
-    if(((getGradus(myTriangle.adjacent_angle) + getGradus(myTriangle.opposite_angle)) != 90) && (getGradus(myTriangle.adjacent_angle) != 0 && getGradus(myTriangle.adjacent_angle) != 0)){
-        return false;
-    }
-    if(getGradus(myTriangle.adjacent_angle) <0 || getGradus(myTriangle.opposite_angle) < 0 || myTriangle.hypotenuse <0 || myTriangle.firstleg <0 || myTriangle.secondleg < 0){
-        return false;
-    }
-    return true;
-}
-
-
-
-
-
-//firstleg - протилежний катет від гіпотенузи
-//secondleg - прилеглий катет
-
-//opposite - протилежний від катета кут
-//adjansed - прилеглий до катета кут
-function getDataWithHypotenuse(triangle)
-{
-    if(triangle.firstleg != 0){
-        triangle.secondleg = Math.sqrt((triangle.hypotenuse * triangle.hypotenuse) - (triangle.firstleg * triangle.firstleg));
-       triangle.adjacent_angle = Math.atan(triangle.secondleg/triangle.firstleg);
-       triangle.opposite_angle =  Math.atan(triangle.firstleg/triangle.secondleg);
-       show(triangle);
-        return;
-    }
-    if(triangle.opposite_angle != 0){
-        triangle.adjacent_angle = 90 - getGradus(myTriangle.opposite_angle);
-        triangle.firstleg = triangle.hypotenuse * Math.sin(myTriangle.opposite_angle);
-        triangle.secondleg = triangle.hypotenuse * Math.sin(getRadian(myTriangle.adjacent_angle));
-        show(triangle);
-        return;
-    }
-}
-function  getDataWithoutHipotenuse(triangle){
-    if(triangle.firstleg != 0 && triangle.secondleg != 0){
-        triangle.hypotenuse = Math.sqrt((triangle.firstleg * triangle.firstleg)+(triangle.secondleg * triangle.secondleg));
-        triangle.adjacent_angle = Math.atan(triangle.secondleg/triangle.firstleg);
-        triangle.opposite_angle =  Math.atan(triangle.firstleg/triangle.secondleg);
-        show(triangle);
-        return;
-    }
-    if(triangle.adjacent_angle != 0 && triangle.firstleg != 0){
-        triangle.hypotenuse = triangle.firstleg / Math.cos(triangle.adjacent_angle);
-        triangle.secondleg = triangle.firstleg * Math.tan(triangle.adjacent_angle);
-        triangle.opposite_angle = 90 - getGradus(myTriangle.adjacent_angle);
-        show(triangle);
-        return;
-    }
-    if(triangle.opposite_angle != 0 && triangle.firstleg != 0){
-        triangle.hypotenuse = triangle.firstleg/Math.sin(triangle.opposite_angle);
-        triangle.adjacent_angle = 90 - getGradus(myTriangle.opposite_angle);
-        triangle.secondleg = triangle.firstleg * Math.tan(getRadian(myTriangle.adjacent_angle));
-        show(triangle);
-        return;
-    }
-
-}
-function show(triangle){
-    console.log("Результат:");
-    console.log("Гіпотенуза " + triangle.hypotenuse);
-    console.log("Протилежний катет" + triangle.firstleg);
-    console.log("Прилеглий катет " + triangle.secondleg);
-    if(triangle.opposite_angle > 10){
-        console.log("Протилежний від катета кут "+ triangle.opposite_angle);
-    }
-    else{
-        console.log("Протилежний від катета кут "+ getGradus(myTriangle.opposite_angle));
-    }
-    if(triangle.adjacent_angle>10){
-        console.log("Прилеглий до катета кут " + triangle.adjacent_angle);
-    }
-    else{
-        console.log("Прилеглий до катета кут " + getGradus(myTriangle.adjacent_angle));
-    }
-
+    console.log("a = " + a + "\nb = " + b + "\nc = " + c + "\nalpha = " + alpha + "\nbeta = " + beta + "\nsuccess!");
 }
